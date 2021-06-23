3a403a0afad494ab4c7d0406765384dcdaa3ef78
Robertdag03 <robertdag03@gmail.com>
2021-06-23 01:13:32 -0400

    Added ability to buy consumers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 var matter = 0;
+const consumers = [0, 0, 0, 0, 0, 0, 0, 0];
+
 function GatherClicked(){
   matter = matter + 1;
-  document.getElementById("matterDisplay").innerHTML = "You Have " + matter + " Matter";
 }
 
 function Tick(){
+  document.getElementById("matterDisplay").innerHTML = "You Have " + matter + " Matter";
+}
 
+function Buy(type, mk){
+  if(type == 1){
+    consumers[mk] += 1;
+  }
+}
+
+function Dbug(){
+  var outer = "";
+  for(var i = 0; i < 8; i++){
+    outer += consumers[i] + " ";
+  }
+  console.log(outer);
 }
