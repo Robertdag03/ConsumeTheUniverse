66ff272190c8923e1f1f8def20e01180db81fd3d
Robertdag03 <robertdag03@gmail.com>
2021-06-23 01:24:25 -0400

    Eh buying system. Also consumers need work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var matter = 0;
+var ticker = 0;
 const consumers = [0, 0, 0, 0, 0, 0, 0, 0];
+const consumerPrices = [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000];
 
 function GatherClicked(){
   matter = matter + 1;
@@ -7,11 +9,19 @@ function GatherClicked(){
 
 function Tick(){
   document.getElementById("matterDisplay").innerHTML = "You Have " + matter + " Matter";
+  ticker += 1;
+  if(ticker >= 100){
+    UpdateValues();
+    ticker = 0;
+  }
 }
 
 function Buy(type, mk){
   if(type == 1){
-    consumers[mk] += 1;
+    if(matter >= consumerPrices[mk]){
+      consumers[mk] += 1;
+      matter -= consumerPrices[mk];
+    }
   }
 }
 
@@ -21,4 +31,12 @@ function Dbug(){
     outer += consumers[i] + " ";
   }
   console.log(outer);
+}
+
+function UpdateValues(){
+  for(var i = 0; i < 8; i++){
+    if(consumers[i] > 1){
+      matter += consumers[i] * ((10^i)/(7-i));
+    }
+  }
 }
